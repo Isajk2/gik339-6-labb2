a246c1ab692afa330358c0c036a6356fbfb13644
c33k4y <h22cekar@du.se>
2023-12-21 09:25:09 +0100

    ändrat script/html/css
    men får inte igång eller åtkomst till servern så scriptet kan inte hämta datan?

--- gik339-ht23-Labb2/client/script.js
+++ gik339-ht23-Labb2/client/script.js
@@ -1,3 +1,64 @@
+const url = "http://localhost:3000/users";
+
+async function fetchUserData() {
+  try {
+    const response = await fetch(url);
+    const userData = await response.json();
+    console.log(userData);
+    return userData;
+  } catch (error) {
+    console.error("Error:", error);
+    return [];
+  }
+}
+
+async function displayUserData() {
+  try {
+    const users = await fetchUserData();
+    const userTable = document.createElement("table");
+    userTable.classList.add("user-table");
+
+    const tableHead = document.createElement("tr");
+    tableHead.innerHTML = 
+    `
+      <th>ID</th>
+      <th>Användarnamn</th>
+      <th>Förnamn</th>
+      <th>Efternamn</th>
+      <th>Färg</th>
+    `
+    ;
+    
+    userTable.appendChild(tableHead);
+
+    users.forEach((user) => {
+      const userRow = document.createElement("tr");
+      userRow.innerHTML = 
+      `
+        <td>${user.id}</td>
+        <td>${user.username}</td>
+        <td>${user.firstName}</td>
+        <td>${user.lastName}</td>
+        <td style="background-color: ${user.color};">${user.color}</td>
+      `
+      ;
+      userTable.appendChild(userRow);
+    });
+
+    const section = document.createElement("section");
+    section.classList.add("user-section");
+    section.appendChild(userTable);
+
+    const main = document.querySelector("main");
+    main.appendChild(section);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+displayUserData();
+
+
+/*
 async function fetchData() {
     const url = 'http://localhost:3000/users';
   
@@ -19,7 +80,32 @@ async function fetchData() {
       // Part 7: Console.log for verification
       console.log(userArray);
   
-      // Add your code here to work with the userArray...
+      // Skapar och hämtar lista
+      const userList = document.createElement('div');
+      userList.className = 'user-list';
+
+      // Loopar användare och skapar item
+      users.forEach(user => {
+          const userItem = document.createElement('div');
+          userItem.className = `user-list ${user.color}`;
+
+          // Använder templatesträng och infogar användardata
+          userItem.innerHTML = `
+              ${user.firstName} ${user.lastName}
+              Användarnamn: ${user.username}
+              Favoritfärg: ${user.color}
+          `;
+
+          // Lägger till användare i listan
+          userList.appendChild(userItem);
+      });
+
+      // Lägger till i DOM-trädet
+      document.getElementById('user-list').appendChild(userList);
+  } catch (error) {
+      console.error('Error:', error);
+  }
+});
   
     } catch (error) {
       console.error('Error fetching data:', error);
@@ -27,3 +113,13 @@ async function fetchData() {
   }
   
   fetchData();
+
+  document.addEventListener('DOMContentLoaded', async () => {
+    try {
+        const response = await fetch('http://localhost:3000/users');
+        if (!response.ok) {
+            throw new Error('Nätverksresponsen var inte okej');
+        }
+        const users = await response.json(); */
+
+        
--- gik339-ht23-Labb2/server/server.js
+++ gik339-ht23-Labb2/server/server.js
@@ -1,3 +1,35 @@
+const express = require("express");
+const server = express();
+const sqlite3 = require("sqlite3").verbose();
+
+server
+  .use(express.json())
+  .use(express.urlencoded({ extended: false }))
+  .use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Allow-Methods", "*");
+    next();
+  });
+
+server.get("/users", (req, res) => {
+  const db = new sqlite3.Database("./gik339-labb2.db");
+
+  db.all("SELECT * FROM users", (err, rows) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.send(rows);
+    }
+  });
+});
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Servern är aktiv på: ${port}`);
+});
+
+/*
 const express = require('express');
 const server = express();
 const sqlite3 = require('sqlite3').verbose(); // Import sqlite3 package
@@ -38,6 +70,7 @@ server.get('/users', (req, res) => {
   });
   
   // Starta servern
-  server.listen(port, () => {
+  server.listen(port 3000, () => {
     console.log(`Server is listening on port ${port}`);
   });
+  */
